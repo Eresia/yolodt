e25864128a4d60eb84adeb05a3a533216c0a945d
Julien Abadji <sudo.screenlife@gmail.com>
2014-11-26 11:47:57 +0100

    FileDocument works

---
+++ src/Document.java
@@ -0,0 +1,51 @@
+import java.io.Serializable;
+
+
+public class Document implements Serializable{
+
+	private String path;
+	
+	public Document(String path)
+	{
+		this.path = path;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String toString()
+	{
+		return "path : " + path;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Document other = (Document) obj;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/FileDocument.java
@@ -0,0 +1,128 @@
+import java.io.BufferedInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class FileDocument {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) {
+
+		File folder = openTmp("/home/eresia/Documents");
+		ArrayList<Document> docs1 = new ArrayList<Document>();
+		ArrayList<Document> docs2;
+		for (int i = 0; i < 5; i++) {
+			docs1.add(new Document("coucou " + Integer.toString(i)));
+		}
+		// System.out.println(docs1);
+		if (write(docs1, folder)) {
+			docs2 = (ArrayList<Document>) read(folder);
+			try {
+				for (Document doc : docs2) {
+					System.out.println(doc.toString());
+				}
+			} catch (NullPointerException e) {
+				System.out.println("docs2 null");
+			}
+		}
+	}
+
+	public static ArrayList read(File folder) {
+		ObjectInputStream ois;
+		ArrayList objs = new ArrayList();
+		Object reading;
+		try {
+			ois = new ObjectInputStream(new BufferedInputStream(
+					new FileInputStream(folder.getAbsolutePath()
+							+ File.separator + "data.db")));
+			try {
+				while (true) {
+					reading = ois.readObject();
+					objs.add(reading);
+				}
+			} catch (EOFException e) {
+				ois.close();
+				return objs;
+			} catch (ClassNotFoundException e) {
+				System.out.println("Class no exist (in read)");
+				e.printStackTrace();
+				return null;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found (in read)");
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			System.out.println("IOException (in read)");
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean write(ArrayList objs, File folder) {
+		ObjectOutputStream oos;
+		if (folder.isDirectory()) {
+			try {
+				oos = new ObjectOutputStream(new FileOutputStream(
+						folder.getAbsolutePath() + File.separator + "data.db"));
+				for (Object obj : objs) {
+					oos.writeObject(obj);
+				}
+				oos.close();
+				return true;
+			} catch (FileNotFoundException e) {
+				System.out.println("File not found");
+				e.printStackTrace();
+				return false;
+			} catch (IOException e) {
+				System.out.println("Error in Write");
+				e.printStackTrace();
+				return false;
+			}
+		} else {
+			System.out.println(folder.getAbsolutePath() + " is a directory");
+			return false;
+		}
+	}
+
+	public static File openTmp(String path) {
+		File folder;
+
+		try {
+			folder = new File(path + File.separator + "tmp" + File.separator);
+			if (!(folder.exists())) {
+				folder.mkdir();
+			} else {
+				deleteFolder(folder);
+				folder.mkdir();
+			}
+			return folder;
+		} catch (Exception e) {
+			System.out.println("Erreur sur openTmp");
+			return null;
+		}
+
+	}
+
+	public static void deleteFolder(File file) {
+		if (file.isDirectory()) {
+			for (int i = 0; i < file.list().length; i++) {
+				deleteFolder(new File(file.getAbsolutePath() + File.separator
+						+ file.list()[i]));
+			}
+			file.delete();
+		} else {
+			file.delete();
+		}
+	}
+
+}
