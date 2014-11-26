4e8dbf27feb66aafb7f837c078be486bae758d04
Julien Abadji <sudo.screenlife@gmail.com>
2014-11-26 11:38:07 +0100

    rm XmlParser.java

--- XmlParser.java
+++
@@ -1,7 +0,0 @@
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-
-public class XmlParser {
-	final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	
-}
