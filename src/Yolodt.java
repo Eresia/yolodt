2f7dca21bfe78f3ff6dfa0c903cc9ec3e419700e
Julien Abadji <sudo.screenlife@gmail.com>
2014-12-28 16:58:32 +0100

    del FileDocument.java

--- src/Yolodt.java
+++ src/Yolodt.java
@@ -9,11 +9,12 @@ public class Yolodt {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Folder folderToStudy = new Folder("res/"); //On balance le dossier à étudier
-		File tmp = folderToStudy.createTmp(folderToStudy.getPath());
-		ArrayList<Document> odtToStudy = folderToStudy.searchODT(folderToStudy.getPath()); //On choppe les ODT dedans
-//		Parser p = new Parser("res/content.xml");
-//		System.out.println(p);
+//		Folder folderToStudy = new Folder("res/"); //On balance le dossier à étudier
+//		File tmp = folderToStudy.createTmp(folderToStudy.getPath());
+//		ArrayList<Document> odtToStudy = folderToStudy.searchODT(folderToStudy.getPath()); //On choppe les ODT dedans
+//		
+		Parser p = new Parser("res/content.xml");
+		System.out.println(p);
 		
 	}
 }
